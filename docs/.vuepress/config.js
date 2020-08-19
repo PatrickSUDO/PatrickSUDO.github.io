5b2e0bc7e1545f04f5dc812f6d1e77ae8f5e140f
PatrickSUDO <patricksuph@gmail.com>
2020-08-19 22:10:56 +0800

    fix comment 2

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -31,7 +31,7 @@ module.exports = {
       '/': {
         vssueConfig: {
           locale: 'en-US',
-          platform: 'github-v4',
+          platform: 'github',
           owner: 'PatrickSUDO',
           repo: 'comment',
           clientId: '64ebf58d65249e13ffb2',
